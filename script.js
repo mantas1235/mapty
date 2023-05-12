f3ce5cb09e04dfc7fdd7937dbe011acaadefd766
Mantas Santockis <mantas.san@gmail.com>
2023-05-12 08:54:44 +0300

    making form of workout apear

--- script.js
+++ script.js
@@ -12,24 +12,72 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 
+let map, mapEvent;
+
 if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function(position) {
     const {latitude} = position.coords;
     const {longitude} = position.coords
-console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+// console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
 const coords = [latitude, longitude]
 
-const map = L.map('map').setView(coords, 13);
+map = L.map('map').setView(coords, 13);
+
+// console.log(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+
+
+
+map.on("click", function (mapE) {
+    mapEvent = mapE
+
+form.classList.remove('hidden');
+inputDistance.focus()
+
+
+
+})
+
 
 }, function() {
     alert(`Could not get Your possition`)
+}
+);
+
+
+
+form.addEventListener("submit", function(e){
+    //Display marker
+
+    e.preventDefault()
+
+//clear input fields
+
+inputDistance.value= inputDuration.value = inputCadence.value = inputElevation.value = ""
+
+const{lat,lng} = mapEvent.latlng;
+
+    L.marker([lat, lng]).addTo(map)
+    .bindPopup(L.popup({
+        maxWidth:250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup"
+    }))
+    .setPopupContent("Workout")
+    .openPopup();
+})
+
+
+inputType.addEventListener("change", function(e){
+e.preventDefault()
+inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+inputCadence.closest(".form__row").classList.toggle("form__row--hidden")
+
 })
